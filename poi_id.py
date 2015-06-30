347c8c7bb35d2e0b46b8a6e9593a29e61ce6b0c5
Mohit Joshi <animusmojo@gmail.com>
2015-07-01 00:12:17 +0530

    Features list updated, F1 score improved.

--- poi_id.py
+++ poi_id.py
@@ -10,12 +10,21 @@ from tester import test_classifier, dump_classifier_and_data
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
+'''
 features_list = ['poi', 'salary', 'to_messages', 'deferral_payments', 'total_payments',
                  'loan_advances', 'bonus', 'restricted_stock_deferred',
                  'deferred_income', 'total_stock_value', 'expenses', 'from_poi_to_this_person',
                  'exercised_stock_options', 'from_messages', 'other', 'from_this_person_to_poi',
                  'long_term_incentive', 'shared_receipt_with_poi', 'restricted_stock',
                  'director_fees'] # You will need to use more features
+'''
+features_list = ['poi', 'salary', 'to_messages', 'deferral_payments', 'total_payments',
+                  'bonus', 'restricted_stock_deferred',
+                 'deferred_income', 'total_stock_value', 'expenses', 'from_poi_to_this_person',
+                 'from_messages',  'from_this_person_to_poi',
+                 'long_term_incentive', 'restricted_stock'] # You will need to use more features
+
+
 
 ### Load the dictionary containing the dataset
 data_dict = pickle.load(open("final_project_dataset.pkl", "r") )
@@ -29,6 +38,7 @@ for key in data_dict:
 data_dict.pop('TOTAL') # As 'TOTAL  is clearly an outlier'
 
 ### Task 3: Create new feature(s)
+'''
 for key in data_dict:
     if data_dict[key]['to_messages']==0:
         data_dict[key]['to_fraction'] = 0
@@ -39,7 +49,7 @@ for key in data_dict:
         data_dict[key]['from_fraction'] = 0
     else:
         data_dict[key]['from_fraction'] = float(data_dict[key]['from_this_person_to_poi'])/(data_dict[key]['from_messages'])
-    
+'''    
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -53,8 +63,14 @@ labels, features = targetFeatureSplit(data)
 ### you'll need to use Pipelines. For more info:
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()    # Provided to give you a starting point. Try a varity of classifiers.
+from sklearn import tree
+clf = tree.DecisionTreeClassifier(min_samples_split=42, criterion = 'entropy')    # Provided to give you a starting point. Try a varity of classifiers.
+
+'''
+from sklearn.svm import SVC
+c=10000.0 # this value is tuned, large c is giving more accuracy,tried c=10, c = 100, 1000 etc
+clf = SVC(kernel='linear', C=c)
+'''
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall 
 ### using our testing script.
